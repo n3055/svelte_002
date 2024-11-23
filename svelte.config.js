8dd086c6bcad5b6f845183e92d20510c02926c35
n3055 <0x30n3055@gmail.com>
2024-11-23 23:12:12 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   kit: {
     adapter: vercel({
-      runtime: 'nodejs18.x' // Explicitly set the Node.js runtime to version 18
+      // Explicitly set the Node.js runtime to version 18
     })
   },
   preprocess: vitePreprocess()
